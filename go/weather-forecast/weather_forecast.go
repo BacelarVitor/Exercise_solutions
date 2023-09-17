b5868b5972384be5f13142a00f42879bf81d6736
Vitor Bacelar de Paula Augusto <vitor.bacelar@protonmail.com>
2023-09-16 22:20:07 -0300

    learn about godoc

--- go/weather-forecast/weather_forecast.go
+++ go/weather-forecast/weather_forecast.go
@@ -1,8 +1,13 @@
+// Package weather provides tools to see the current forecast of Globinocus cities.
 package weather
 
+// CurrentCondition represents the current forecast of the city.
 var CurrentCondition string
+
+// CurrentLocation represents the current selected city for the forecast.
 var CurrentLocation string
 
+// Forecast receives a string value called city that represents the city for the forecast and a string value condition that represents the current weather of the selected city and returns a string value representing the current forecast of the selected city.
 func Forecast(city, condition string) string {
 	CurrentLocation, CurrentCondition = city, condition
 	return CurrentLocation + " - current weather condition: " + CurrentCondition
