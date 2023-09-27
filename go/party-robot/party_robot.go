9f4e85270b32e509d6e0402dc137a1eda4b53486
Vitor Bacelar de Paula Augusto <vitor.bacelar@protonmail.com>
2023-09-27 16:38:47 -0300

    finish party_robot

--- go/party-robot/party_robot.go
+++ go/party-robot/party_robot.go
@@ -4,7 +4,7 @@ import "fmt"
 
 // Welcome greets a person by name.
 func Welcome(name string) string {
-	return fmt.Sprintf("Welcome to my party, %s", name)
+	return fmt.Sprintf("Welcome to my party, %s!", name)
 }
 
 // HappyBirthday wishes happy birthday to the birthday person and exclaims their age.
@@ -14,5 +14,6 @@ func HappyBirthday(name string, age int) string {
 
 // AssignTable assigns a table to each guest.
 func AssignTable(name string, table int, neighbor, direction string, distance float64) string {
-	panic("Please implement the AssignTable function")
+	return fmt.Sprintf("%s\nYou have been assigned to table %03d. Your table is %s, exactly %.1f meters from here.\nYou will be sitting next to %s.",
+		Welcome(name), table, direction, distance, neighbor)
 }
