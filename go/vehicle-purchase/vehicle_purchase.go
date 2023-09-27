554775d3a7e4645d1a7fc4674dae64a3fb69738b
Vitor Bacelar de Paula Augusto <vitor.bacelar@protonmail.com>
2023-09-27 16:48:57 -0300

    play with statments

--- go/vehicle-purchase/vehicle_purchase.go
+++ go/vehicle-purchase/vehicle_purchase.go
@@ -7,13 +7,20 @@ func NeedsLicense(kind string) bool {
 
 // ChooseVehicle recommends a vehicle for selection. It always recommends the vehicle that comes first in lexicographical order.
 func ChooseVehicle(option1, option2 string) string {
+	text := " is clearly the better choice."
 	if option1 < option2 {
-		return option1
+		return option1 + text
 	}
-	return option2
+	return option2 + text
 }
 
 // CalculateResellPrice calculates how much a vehicle can resell for at a certain age.
 func CalculateResellPrice(originalPrice, age float64) float64 {
-	panic("CalculateResellPrice not implemented")
+	if age < 3 {
+		return originalPrice * 0.8
+	}
+	if age < 10 {
+		return originalPrice * 0.7
+	}
+	return originalPrice * 0.5
 }
