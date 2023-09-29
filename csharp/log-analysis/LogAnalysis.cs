454463e5df9c418dac3ac8ab772874f506576b51
Vitor Bacelar de Paula Augusto <vitor.bacelar@protonmail.com>
2023-09-29 17:42:27 -0300

    log analysis

--- csharp/log-analysis/LogAnalysis.cs
+++ csharp/log-analysis/LogAnalysis.cs
@@ -3,10 +3,27 @@ using System;
 public static class LogAnalysis 
 {
     // TODO: define the 'SubstringAfter()' extension method on the `string` type
+    public static string SubstringAfter(this string str,  string substring) 
+    {
+        var index = str.IndexOf(substring)+substring.Length;
+        return str[index..];
+    }
 
     // TODO: define the 'SubstringBetween()' extension method on the `string` type
-    
+    public static string SubstringBetween(this string str, string firstSub, string secondSub)
+    {
+        var firstIndex = str.IndexOf(firstSub)+firstSub.Length;
+        var idealLenght = str.LastIndexOf(secondSub) - firstIndex;
+        return str.Substring(firstIndex, idealLenght);
+    }
     // TODO: define the 'Message()' extension method on the `string` type
-
+    public static string Message(this string str) 
+    {
+        return str.SubstringAfter(": ");
+    }
     // TODO: define the 'LogLevel()' extension method on the `string` type
+    public static string LogLevel(this string str) 
+    {
+        return str.SubstringBetween("[", "]");
+    }
 }
