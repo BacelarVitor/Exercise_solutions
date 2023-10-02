4de3b21466340855dc25687ffec98eacda46878e
Vitor Bacelar de Paula Augusto <vitor.bacelar@protonmail.com>
2023-10-02 17:05:32 -0300

    get item

--- go/card-tricks/card_tricks.go
+++ go/card-tricks/card_tricks.go
@@ -8,7 +8,7 @@ func FavoriteCards() []int {
 // GetItem retrieves an item from a slice at given position.
 // If the index is out of range, we want it to return -1.
 func GetItem(slice []int, index int) int {
-	panic("Please implement the GetItem function")
+	return slice[index]
 }
 
 // SetItem writes an item to a slice at given position overwriting an existing value.
